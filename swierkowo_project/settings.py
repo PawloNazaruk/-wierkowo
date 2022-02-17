8cccb2f26634de4665525a66193bc701d19bdafe
PawloNazaruk <pawlonazaruk@gmail.com>
2022-02-17 01:23:25 +0100

    Footer added missing font import

--- swierkowo_project/settings.py
+++ swierkowo_project/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = ''
+
+print(f'{BASE_DIR=}')
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print(f'{STATIC_ROOT=}')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
